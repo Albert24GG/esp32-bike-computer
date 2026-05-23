87ac50fc54f29fd8660272bdaca3a7c62c2a3aa4
Albert24GG <guiman.albert09@gmail.com>
2026-05-24 01:13:16 +0300

    Add the Squareline Studio project

---
+++ squareline_project/bike_computer_ui_events.py
@@ -0,0 +1,24 @@
+
+def cb_exit_screen_settings(event_struct):
+    return
+
+
+def cb_reset_trip(event_struct):
+    return
+
+
+def cb_ble_start_pairing(event_struct):
+    return
+
+
+def ble_cancel_pairing(event_struct):
+    return
+
+
+def cb_exit_screen_maps(event_struct):
+    return
+
+
+def cb_ble_cancel_pairing(event_struct):
+    return
+
